90be82ab49f89df78b951b0d11e055931ad987db
globz-eu <16118065+globz-eu@users.noreply.github.com>
2018-04-10 12:02:52 +0200

    Add shared components

---
+++ styled-components/globalStyle.js
@@ -0,0 +1,69 @@
+import { injectGlobal } from "styled-components"
+
+
+const fontFamily = "Cabin"
+
+export function injectGlobalStyle(fontServer) {
+  injectGlobal`
+    body {
+      margin: 0;
+      padding: 0;
+    }
+
+    @font-face {
+      font-family: ${fontFamily};
+      font-weight: 400;
+      font-style: normal;
+      src: url("${fontServer}/Cabin/Cabin-Regular.ttf");
+    }
+
+    @font-face {
+      font-family: ${fontFamily};
+      font-weight: 400;
+      font-style: italic;
+      src: url("${fontServer}/Cabin/Cabin-Italic.ttf");
+    }
+
+    @font-face {
+      font-family: ${fontFamily};
+      font-weight: 500;
+      font-style: normal;
+      src: url("${fontServer}/Cabin/Cabin-Medium.ttf");
+    }
+
+    @font-face {
+      font-family: ${fontFamily};
+      font-weight: 500;
+      font-style: italic;
+      src: url("${fontServer}/Cabin/Cabin-MediumItalic.ttf");
+    }
+
+    @font-face {
+      font-family: ${fontFamily};
+      font-weight: 600;
+      font-style: normal;
+      src: url("${fontServer}/Cabin/Cabin-SemiBold.ttf");
+    }
+
+    @font-face {
+      font-family: ${fontFamily};
+      font-weight: 600;
+      font-style: italic;
+      src: url("${fontServer}/Cabin/Cabin-SemiBoldItalic.ttf");
+    }
+
+    @font-face {
+      font-family: ${fontFamily};
+      font-weight: 700;
+      font-style: normal;
+      src: url("${fontServer}/Cabin/Cabin-Bold.ttf");
+    }
+
+    @font-face {
+      font-family: ${fontFamily};
+      font-weight: 700;
+      font-style: italic;
+      src: url("${fontServer}/Cabin/Cabin-BoldItalic.ttf");
+    }
+  `
+}
