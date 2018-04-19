253046ab67863e0206c629939a2fd372109388f4
Stefan Dieterle <stefand@artcom.de>
2018-04-19 10:12:06 +0200

    Update title style

--- styled-components/components/pageTitle.js
+++ styled-components/components/pageTitle.js
@@ -1,9 +1,22 @@
+import React from "react"
 import styled from "styled-components"
 
-export default styled.div`
+const Container = styled.div`
   align-self: center;
+  height: 3.5em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const Title = styled.div`
   font-size: ${props => props.theme.font.headline.size};
   font-weight: ${props => props.theme.font.headline.weight};
   color: ${props => props.theme.font.headline.color};
   text-align: center;
 `
+
+export default props =>
+  <Container>
+    <Title>{ props.text }</Title>
+  </Container>
