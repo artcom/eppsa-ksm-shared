92934acc75e8d12b8069b132076d0816f6f66320
globz-eu <16118065+globz-eu@users.noreply.github.com>
2018-04-24 16:55:21 +0200

    Add x scale parameter to clickEffect

--- animations/clickEffect.js
+++ animations/clickEffect.js
@@ -1,17 +1,18 @@
 import { css, keyframes } from "styled-components"
 
-export default function clickEffect() {
+export default function clickEffect(initialXScale = 1) {
+  console.log(initialXScale)
   const click = keyframes`
     0% {
-      transform: scale(1, 1);
+      transform: scale(${initialXScale}, 1);
       opacity: 1;
     }
     50%   {
-      transform: scale(0.9, 0.9);
+      transform: scale(${initialXScale * 0.9}, 0.9);
       opacity: 0.5;
     }
     100% {
-      transform: scale(1, 1);
+      transform: scale(${initialXScale}, 1);
       opacity: 1;
     }
   `
