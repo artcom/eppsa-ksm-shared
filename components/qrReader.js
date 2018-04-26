0fd8af29a65c23c8e07423b70d958a786af1b8b8
Stefan Dieterle <stefand@artcom.de>
2018-04-26 13:30:20 +0200

    Make qrReader size customizable

--- components/qrReader.js
+++ components/qrReader.js
@@ -6,8 +6,8 @@ import QrReaderSeeker from "../assets/EPPSA_Assets_QR-Code_Scanner.svg"
 
 const QrReaderContainer = styled.div`
   box-sizing: border-box;
-  width: ${props => props.theme.layout.cardWidth * 0.7}vw;
-  height: ${props => props.theme.layout.cardWidth * 0.7}vw;
+  width: ${props => props.theme.layout.cardWidth * props.size}vw;
+  height: ${props => props.theme.layout.cardWidth * props.size}vw;
   padding: ${props => props.theme.layout.mediumSpacing}vw;
   align-self: center;
   background-color: ${props => props.theme.colors.secondary};
@@ -18,11 +18,13 @@ const StyledQrReaderSeeker = styled(QrReaderSeeker)`
   position: absolute;
   z-index: 2;
   transform: translate(
-    ${props => props.theme.layout.cardWidth * 0.15 / 2 - props.theme.layout.mediumSpacing}vw,
-    ${props => props.theme.layout.cardWidth * 0.15 / 2 - props.theme.layout.mediumSpacing}vw
+    ${props =>
+    props.theme.layout.cardWidth * (props.size * 0.2) / 2 - props.theme.layout.mediumSpacing}vw,
+    ${props =>
+    props.theme.layout.cardWidth * (props.size * 0.2) / 2 - props.theme.layout.mediumSpacing}vw
   );
-  width: ${props => props.theme.layout.cardWidth * 0.55}vw;
-  height: ${props => props.theme.layout.cardWidth * 0.55}vw;
+  width: ${props => props.theme.layout.cardWidth * props.size * 0.8}vw;
+  height: ${props => props.theme.layout.cardWidth * props.size * 0.8}vw;
   fill: ${props => props.color};
 `
 
@@ -34,11 +36,11 @@ const StyledQrReader = styled(ReactQrReader)`
 `
 
 export default function QrReader(props) {
-  const { background, seekerColor } = props
+  const { background, seekerColor, size } = props
 
   return (
-    <QrReaderContainer background={ background }>
-      <StyledQrReaderSeeker color={ seekerColor } />
+    <QrReaderContainer background={ background } size={ size }>
+      <StyledQrReaderSeeker color={ seekerColor } size={ size } />
       <StyledQrReader
         onScan={ props.onScan }
         onError={ props.onError }
