a9ff73262e5092376b54f56924708e9f86e92410
Mirko Geissler <mirko.geissler@artcom.de>
2018-04-30 11:15:03 +0200

    Use children in pageTtitle

--- components/pageTitle.js
+++ components/pageTitle.js
@@ -2,13 +2,10 @@ import React from "react"
 import styled from "styled-components"
 
 
-const Title = styled.div`
+export default styled.div`
   font-size: ${props => props.theme.font.headline.size}vw;
   font-weight: ${props => props.theme.font.headline.weight};
   color: ${props => props.theme.font.headline.color};
   text-align: center;
   margin-bottom: ${props => props.theme.layout.largeSpacing}vw;
 `
-
-export default props =>
-  <Title>{ props.text }</Title>
