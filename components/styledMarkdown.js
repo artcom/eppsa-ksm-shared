269018383fa962f0c54b667702aa191b5969bca8
Mirko Geissler <mirko.geissler@artcom.de>
2018-05-04 15:05:06 +0200

    Improve markdown layout

--- components/styledMarkdown.js
+++ components/styledMarkdown.js
@@ -5,6 +5,14 @@ export default styled(ReactMarkdown)`
   h1, h2, h3, h4, h5, h6 {
     font-size: 1.1em;
     color: ${props => props.theme.colors.primaryFont};
+    margin-bottom: 0;
+  }
+
+  p {
+    margin-top: 0;
+    :last-child {
+      margin-bottom: 0;
+    }
   }
 
   font-size: ${props => props.theme.font.text.size}vw;
